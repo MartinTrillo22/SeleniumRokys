ba0f93f41a3c4cd0f5ab6651f299e21d4418ecc5
Miguel Angel <miguelgarcia241296@gmail.com>
2026-04-10 19:25:31 -0500

    feat:agregue la config de la pagina de menu y el test para el carrito

--- src/test/java/tests/RokysTest.java
+++ src/test/java/tests/RokysTest.java
@@ -1,10 +1,16 @@
 package tests;
 
-import config.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import config.DriverFactory;
 import pages.LoginPage;
+import pages.MenuPage;
 
 public class RokysTest {
     private WebDriver driver;
@@ -25,7 +31,8 @@ public class RokysTest {
         loginPage.hacerLogin("correo_falso@test.com", "claveIncorrecta123");
 
         String mensajeError = loginPage.obtenerMensajeError();
-        Assert.assertTrue(mensajeError.contains("Credenciales invalidas"), "El sistema no bloqueó el acceso o no mostró el error.");
+        Assert.assertTrue(mensajeError.contains("Credenciales invalidas"),
+                "El sistema no bloqueó el acceso o no mostró el error.");
     }
 
     @Test(priority = 2)
@@ -36,6 +43,16 @@ public class RokysTest {
         Assert.assertTrue(estaLogueado, "Error: El login no fue exitoso, no se encontró el perfil del usuario.");
     }
 
+    @Test(priority = 3)
+    public void testFlujoCompraCompletoSinPago() {
+
+        menuPage.navegarAInicio();
+        menuPage.seleccionarProducto();
+        menuPage.configurarDireccionDelivery("Av. Petit Thouars 116 Lima, Perú", "primer piso", "cerca del parque");
+        menuPage.confirmarProductoEnModal();
+        boolean estamosEnCheckout = menuPage.verificarEstamosEnCheckout();
+        Assert.assertTrue(estamosEnCheckout, "Fallo la prueba: No se pudo llegar a la pantalla final de Checkout.");
+    }
 
     @AfterMethod
     public void tearDown() {
